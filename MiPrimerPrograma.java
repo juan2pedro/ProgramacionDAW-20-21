e49b9c1c984bafc668f7f5963472a83d0b91ed2c
juan2pedro <juanpemt@gmail.com>
2020-10-01 10:53:29 +0200

    actualizacion

---
+++ MiPrimerPrograma.java
@@ -0,0 +1,74 @@
+public class MiPrimerPrograma {
+  public static void main (String[] args) {
+    System.out.println("Hola, es el primer programa.");
+    System.out.println("La suma de 3 y 5 es: ");
+    System.out.print(3+5);
+    System.out.println("");
+    System.out.printf("El numero es: %d.%d \n", 21,45);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
